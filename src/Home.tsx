0eb25ef8cb86fb2d17a6dd88b1854fc0d4ce46e6
Wasan  Awwad <108180628+WA-A@users.noreply.github.com>
2023-12-19 21:36:58 +0200

    TypeScript In React

---
+++ src/Home.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+import { User } from './User'
+
+function Home() {
+    let [users,setUsers]=useState<User[]>();
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
