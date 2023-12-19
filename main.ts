609e9e86de5aed526cb42aa74eebab096efc1d5f
wasan <wasanawwad2020@gmail.com>
2023-12-19 20:38:02 +0200

    Definition of Variables

---
+++ main.ts
@@ -0,0 +1,7 @@
+let UserName:string='WA-A';
+let UserAge:number=21;
+let Bothers:string[]=['Yazan','Aws','Bara'];
+let BAge:number[]=[29,27,25];
+let test:[number|string]=[3]; // This means that it accepts a number or a string
+let data:any; // This means that it accepts any type of data
+let users:any[]; // This means that it array accepts any type of data
