b46cf0ab8faafccabb80e86d1b2ec3534e790193
wasan <wasanawwad2020@gmail.com>
2023-12-19 20:53:21 +0200

    interface & function in ts

--- main.ts
+++ main.ts
@@ -5,3 +5,21 @@ let BAge:number[]=[29,27,25];
 let test:[number|string]=[3]; // This means that it accepts a number or a string
 let data:any; // This means that it accepts any type of data
 let users:any[]; // This means that it array accepts any type of data
+
+
+interface User{
+    name:string,
+    age:number,
+    sayHello():string,
+}
+
+let user:User  = {
+    name:"Wasan",
+    age : 21,
+    sayHello:()=> {
+        return "hello";
+    }
+
+
+}
+console.log(user.name);
